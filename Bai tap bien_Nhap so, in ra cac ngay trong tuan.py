3c5e397791eac36b185abff468bd39b7ca48ebbb
luannt15 <nguyenluan152@gmail.com>
2021-11-18 14:03:33 +0700

    Bai tap bien

---
+++ Bai tap bien_Nhap so, in ra cac ngay trong tuan.py
@@ -0,0 +1,17 @@
+num = int(input("Nhập một số trong khoảng từ 1 đến 7: "))
+if num == 1:
+	print("Monday")
+elif num == 2:
+	print("Tuesday")
+elif num == 3:
+	print("Wednesday")
+elif num == 4:
+	print("Thursday")
+elif num == 5:
+	print("Friday")
+elif num == 6:
+	print("Saturday")
+elif num == 7:
+	print("Sunday")
+else:
+print("Lỗi,số vừa nhập nằm ngoài khoảng từ 1 đến 7")
